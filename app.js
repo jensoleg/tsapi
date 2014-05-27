4c05cfa40cabb1fb690585592b6ed2d8239cf32b
Jens-Ole Graulund <jensole@graulund.net>
2014-05-27 16:35:08 +0200

    Add HTTP mqtt bridge

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express'); 		// call express
 var app = express(); 				// define our app using express
-var bodyParser = require('body-parser');
+var getRawBody = require('raw-body')
 var mongoose = require('mongoose');
 var MTI = require('mongoose-ts');
 var jwt = require('express-jwt');
 var moment = require('moment');
 var _ = require('lodash');
 var config = require('./config.json');
+var mqtt = require('mqtt');
+
+
+var token, mqtt_client;
 
 var port = process.env.PORT || config.port || 8080; 		// set our port
 
@@ -15,23 +19,45 @@ mongoose.connect(config.mongoose.dbURI + config.mongoose.db);
 var formats = {'hash': 'hash', timestamp: '[x,y]', time: '[ms,y]'};
 var intervals = ['1', '60', '3600'];
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser());
-
 var authenticate = jwt({
     secret: new Buffer(config.auth0.clientSecret, 'base64'),
     audience: config.auth0.clientId
 });
 // authenticate every api call
-app.use('/', authenticate);
+app.use('*', authenticate);
 
-app.all('/', function (req, res, next) {
+app.all('*', function (req, res, next) {
     // set origin policy etc so cross-domain access wont be an issue
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,  Content-Type, Accept");
+
+    var token_str = req.headers.authorization.split(" ");
+    token = token_str[1];
+
+    if (!mqtt_client && token)
+        mqtt_client = mqtt.connect('mqtt://JWT:' + token + '@localhost');
+
+    if (!_.isEmpty(req.query)){
+        req.url = '/search' + req.url;
+    } else
+        req.url = '/mqtt';
+
     next();
+});
+
+app.use(function (req, res, next) {
+    getRawBody(req, {
+        length: req.headers['content-length'],
+        limit: '1mb',
+        encoding: 'utf8'
+    }, function (err, string) {
+        if (err)
+            return next(err);
+
+        req.text = string;
+        next()
+    })
 });
 
 var router = express.Router(); 				// get an instance of the express Router
@@ -41,7 +67,7 @@ router.get('/', function (req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-router.route('/api/:device/:sensor', authenticate)
+router.route('/search/:device/:sensor', authenticate)
 
     .get(function (req, res, next) {
 
@@ -113,9 +139,41 @@ router.route('/api/:device/:sensor', authenticate)
 
     });
 
+router.route('/mqtt', authenticate)
+
+    .post(function (req, res, next) {
+        mqtt_client.publish(req.originalUrl, req.text, {retain: false});
+        res.json({ message: 'Post topic', topic: req.originalUrl, payload: req.text});
+    })
+
+    .get(function (req, res, next) {
+
+        var retained_message;
+        mqtt_client
+            .subscribe(req.originalUrl)
+            .on('message', function (topic, message) {
+                retained_message = message;
+                res.json({ message: 'Get', topic: req.originalUrl, payload: message });
+            });
+        setTimeout(function () {
+            if (!retained_message)
+                res.json({ message: 'Topic not available', topic: req.originalUrl});
+        }, 300);
+    })
+
+    .put(function (req, res, next) {
+        mqtt_client.publish(req.originalUrl, req.text, {retain: true});
+        res.json({ message: 'Put topic', topic: req.originalUrl, payload: req.text});
+    })
+
+    .delete(function (req, res, next) {
+        mqtt_client.publish(req.originalUrl, '', {retain: true});
+        res.json({ message: 'Delete topic', topic: req.originalUrl, payload: req.text});
+    });
+
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// all of our routes will be prefixed with
 app.use('/', router);
 
 // Error handler ....
