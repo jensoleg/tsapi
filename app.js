c02547026331c6a7cd4261825f98bc15f2829a20
Jens-Ole Graulund <jensole@graulund.net>
2014-08-10 12:03:35 +0200

    Allow OPTIONS http request

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.all('*', function (req, res, next) {
 
     // set origin policy etc so cross-domain access wont be an issue
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With,  Content-Type, Accept');
 
     if ('OPTIONS' === req.method) {
